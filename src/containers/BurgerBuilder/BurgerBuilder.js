ea4c23364cd5aa0ea333ced359fd47b4244ffd35
Sushant <sushant.adhikari.619@gmail.com>
2020-12-23 11:49:50 +0545

    calculated the ingredient sum dynamically

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,7 +6,7 @@ import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
     state = {
             ingredients: {
-                salad: 1,
+                salad: 2,
                 bacon: 1,
                 cheese: 2,
                 meat: 2
