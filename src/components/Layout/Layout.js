28f104b5e977dc3e6191fe669c6e5dedb901bef8
Sushant <sushant.adhikari.619@gmail.com>
2020-12-25 16:15:12 +0545

    drawer toggle button error fixed and added hamburger icon

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,16 +7,16 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
     }
 
     sideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer: false})
+        this.setState( {showSideDrawer: false} );
     }
 
     sideDrawerToggleHandler = () => {
-        this.setState(( prevState ) => {
-           return {showSideDrawer: !prevState.showSideDrawer};
+        this.setState( ( prevState ) => {
+           return { showSideDrawer: !prevState.showSideDrawer };
         }); 
     }
 
