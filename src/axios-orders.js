6ab0f8bd856923e254c26a1ffd7e55ffa0236df2
Sushant <sushant.adhikari.619@gmail.com>
2020-12-26 10:45:20 +0545

    sent a post request while clicking on the continue button of order summary

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    basURL: 'https://react-burger-builder-3a363-default-rtdb.firebaseio.com/'
+    baseURL: 'https://react-burger-builder-3a363-default-rtdb.firebaseio.com/'
 });
 
 export default instance;
